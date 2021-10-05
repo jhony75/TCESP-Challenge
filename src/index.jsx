fde16fa58f569f8e420d7435b1896c6d1263e202
jhony75 <jonatas.lima.medeiros@live.com>
2021-10-05 02:00:05 -0300

    Branch now functional.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Expenditure from './api/Expenditure'
+import CitiesList from './api/CitiesList'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Expenditure />
+    <CitiesList />
   </React.StrictMode>,
   document.getElementById('root')
 );
